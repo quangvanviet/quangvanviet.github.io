04de6b6ae9c091afce6146dda59b8f774afcd87f
quangvanviet <quangvanviet.mana@gmail.com>
2025-04-03 14:29:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// Import Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
-import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
-
-// Cấu hình Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBT6LriuMIAVKBhOUJX_3d-oVRwklvptDA",
-  authDomain: "mon-33182.firebaseapp.com",
-  databaseURL: "https://mon-33182-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mon-33182",
-  storageBucket: "mon-33182.firebasestorage.app",
-  messagingSenderId: "1023827948484",
-  appId: "1:1023827948484:web:8bdde6dcb4454af42d2a91",
-  measurementId: "G-EF4J5XT43E"
-};
-
-// Khởi tạo Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getDatabase(app);
-
 //Hàm ghi dữ liệu lên Firebase
 function saveDataUserToFirebase() {
       if (!isFinalLoadData) return;
